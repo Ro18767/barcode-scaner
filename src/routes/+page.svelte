d50c749a57f5c35d2a634cba62826fdcad06a760
Ro18767 <timaropw@gmail.com>
2023-07-05 09:26:34 -0700

    added BarcodeDetector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import imgUrl from '$lib/assets/test.jpg?url';
+	import { onMount } from 'svelte';
+	let img: HTMLImageElement;
+</script>
+
+{#if browser && 'BarcodeDetector' in window}
+	<img
+		src={imgUrl}
+		alt="test"
+		on:load={async () => {
+			if (!browser) return;
+			BarcodeDetector.getSupportedFormats().then((supportedFormats) => {
+				supportedFormats.forEach((format) => console.log(format));
+			});
+			const barcodeDetector = new BarcodeDetector();
+			const barcodes = await barcodeDetector.detect(videoEl).catch((error) => {
+				console.error(error);
+				return [];
+			});
+			console.log('barcodes', barcodes);
+		}}
+	/>
+{:else}
+	<h2>BarcodeDetector not supported</h2>
+{/if}
