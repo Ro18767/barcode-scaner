d4cff5362f9a4a84c50fad54fe1b16653b35100b
Ro18767 <timaropw@gmail.com>
2023-07-05 09:50:53 -0700

    changed adapter to static

---
+++ src/routes/+layout.ts
@@ -0,0 +1,7 @@
+export const prerender = true;
+
+// import type { LayoutLoad } from './$types';
+
+// export const load = (async () => {
+//     return {};
+// }) satisfies LayoutLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,26 +3,30 @@
 	import imgUrl from '$lib/assets/test.jpg?url';
 	import { onMount } from 'svelte';
 	let img: HTMLImageElement;
+	async function load() {
+		if (!browser) return;
+		if (!('BarcodeDetector' in window)) return;
+		if (typeof window.BarcodeDetector !== 'function') return;
+		
+		console.log('BarcodeDetector', window.BarcodeDetector);
+
+		// @ts-ignore
+		window.BarcodeDetector.getSupportedFormats().then((supportedFormats) => {
+			// @ts-ignore
+			supportedFormats.forEach((format) => console.log(format));
+		});
+		// @ts-ignore
+		const barcodeDetector = new window.BarcodeDetector();
+		const barcodes = await barcodeDetector.detect(img).catch((error:unknown) => {
+			console.error(error);
+			return [];
+		});
+		console.log('barcodes', barcodes);
+	}
 </script>
 
 {#if browser && 'BarcodeDetector' in window}
-	<img
-		src={imgUrl}
-		alt="test"
-		on:load={async () => {
-			if (!browser) return;
-			console.log('BarcodeDetector', BarcodeDetector);
-			BarcodeDetector.getSupportedFormats().then((supportedFormats) => {
-				supportedFormats.forEach((format) => console.log(format));
-			});
-			const barcodeDetector = new BarcodeDetector();
-			const barcodes = await barcodeDetector.detect(videoEl).catch((error) => {
-				console.error(error);
-				return [];
-			});
-			console.log('barcodes', barcodes);
-		}}
-	/>
+	<img src={imgUrl} alt="test" bind:this={img} on:load={load} />
 {:else}
 	<h2>BarcodeDetector not supported</h2>
 {/if}
